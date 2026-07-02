be7e51bc6c03dbb006d384b0141a929190025cb4
ixchanneamz-hue <ixchanne.amz@gmail.com>
2026-07-02 19:42:31 +0100

    Load database URL from configuration

--- database/db.py
+++ database/db.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://avasty:soufi138693@avasty.mysql.pythonanywhere-services.com/avasty$default"
+from config import DATABASE_URL, require_config
 
 engine = create_engine(
-    DATABASE_URL,
+    require_config(DATABASE_URL, "DATABASE_URL"),
     echo=False,
     pool_pre_ping=True,
     pool_recycle=280
