3721bcc89431e62ab1776f3f0efb9cac4f215f9a
ixchanneamz-hue <ixchanne.amz@gmail.com>
2026-07-09 07:16:24 +0100

    feat(web): expose offerwall postback endpoint

--- falcon_web/core/urls.py
+++ falcon_web/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .postbacks import offerwall_postback
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('tasks/', views.tasks, name='tasks'),
     path('tasks/<int:task_id>/', views.complete_task, name='complete_task'),
     path('withdraw/', views.withdraw, name='withdraw'),
+    path('api/postback/<slug:provider_slug>/', offerwall_postback, name='offerwall_postback'),
 ]
